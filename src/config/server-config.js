e938b3b8ee5ab0b35f27bd3e199af055208e77bf
Ishan-009 <ishanmoorjmalani009@gmail.com>
2024-02-28 17:52:22 +0530

    Implemented mail setup using nodemailer and tested it

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -5,4 +5,6 @@ dotenv.config();
 module.exports = {
   PORT: process.env.PORT,
   FLIGHT_SERVICE: process.env.FLIGHT_SERVICE,
+  GMAIL_PASS: process.env.GMAIL_PASSWORD,
+  GMAIL_EMAIL: process.env.GMAIL_EMAIL,
 };
--- src/index.js
+++ src/index.js
@@ -3,14 +3,27 @@ const express = require("express");
 const apiRoutes = require("./routes/index");
 const app = express();
 const Cron = require("./utils/common/cron-jobs");
+const mailsender = require("./config/email-config");
 app.use(express.json());
 // to help express read json body
 app.use(express.urlencoded({ extended: true }));
 // to read url encodeed stuff
 app.use("/api", apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
   console.log(`Server Listening on port ${ServerConfig.PORT}`);
   Logger.info("Successfully started the server");
-  Cron();
+
+  try {
+    console.log("hi");
+    const response = await mailsender.sendMail({
+      from: ServerConfig.GMAIL_EMAIL,
+      to: "ishanmoorjmalani009@gmail.com",
+      subject: "Service Test Working",
+      text: "Airline Service Testing",
+    });
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
 });
